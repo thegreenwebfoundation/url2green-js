ae81e3a5eb0a3de742a2bf243cb5028ed9ce09c9
Chris Adams <chris@productscience.co.uk>
2024-11-02 09:37:00 +0100

    Satisfy the linter

--- src/hosting-database.test.js
+++ src/hosting-database.test.js
@@ -16,7 +16,7 @@ describe("hostingDatabase", function () {
     "..",
     "data",
     "fixtures",
-    "url2green.test.db",
+    "url2green.test.db"
   );
 
   describe("generating a dump of green domains #dump", function () {
@@ -46,7 +46,7 @@ describe("hostingDatabase", function () {
     test("tries to use a local database if available", async function () {
       const res = await hosting.check(
         ["google.com", "kochindustries.com"],
-        dbPath,
+        dbPath
       );
       expect(res).toContain("google.com");
     });
